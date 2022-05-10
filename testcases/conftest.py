06cefc1a82e22e2a3c98b957a5a50cec61715cca
Mohamad Waqqad <waqqad2012@gmail.com>
2022-05-11 00:49:13 +0300

    fix

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.fixture(autouse=True)
 def setup(request):
-    driver = webdriver.Chrome(executable_path="./drivers/chromedriver.exe")
+    driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
     driver.get("https://petstore.octoperf.com/actions/Catalog.action")
     driver.implicitly_wait(5)
     driver.maximize_window()
