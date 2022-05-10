bed77540bd1ef2a80a051c81839f63c01cd4ad8e
Mohamad Waqqad <waqqad2012@gmail.com>
2022-05-11 00:54:14 +0300

    fix

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,13 +1,35 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(autouse=True)
-def setup(request):
-    driver = webdriver.Chrome(executable_path="./drivers/chromedriver")
+def setup(request, browser):
+    if browser == "chrome":
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    elif browser == "edge":
+        driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    elif browser == "firefox":
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    else:
+        print("browser not found")
     driver.get("https://petstore.octoperf.com/actions/Catalog.action")
     driver.implicitly_wait(5)
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
